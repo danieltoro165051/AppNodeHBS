39c9a5bbdc138b5e444cf78d7094eb299566d686
Daniel Toro <daniel.toro@netwconsulting.com>
2019-07-17 13:33:23 -0500

    Ajuste hbs home

--- hbs/helpers.js
+++ hbs/helpers.js
@@ -1,17 +1,18 @@
 const hbs = require('hbs');
 
-/*  Helpers  */
+
+// helpers
 hbs.registerHelper('getAnio', () => {
     return new Date().getFullYear();
 });
 
 hbs.registerHelper('capitalizar', (texto) => {
 
-    let nombre = texto.toString();
     let palabras = texto.split(' ');
     palabras.forEach((palabra, idx) => {
-        palabra[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+        palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
     });
 
     return palabras.join(' ');
+
 });
